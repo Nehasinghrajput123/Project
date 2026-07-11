c340b660466b39e2568aa009752c56cc6d5071a1
ajay-9815148898 <ajaykbsit@gmail.com>
2026-07-11 23:45:53 +0530

    chnage url

--- project-management-frontend/src/api/axios.js
+++ project-management-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://project-management-backend.onrender.com/api",
+  baseURL: "https://project-backend-f00j.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
